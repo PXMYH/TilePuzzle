2059fece267dce8ad256eb574906fbdd5569df2e
PXMYH <pxmyhdev@gmail.com>
2013-08-27 18:57:04 -0700

    Changed to ListActivity for image display

--- gen/com/px/tilepuzzle/R.java
+++ gen/com/px/tilepuzzle/R.java
@@ -25,17 +25,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Difficulty_Level_1=0x7f070001;
-        public static final int Difficulty_Level_2=0x7f070002;
-        public static final int Difficulty_Level_3=0x7f070003;
-        public static final int alarm_enabled=0x7f07000a;
-        public static final int button_test=0x7f070009;
-        public static final int congrat_msg=0x7f070005;
-        public static final int congrat_msg2=0x7f070007;
-        public static final int difficulty_list=0x7f070000;
-        public static final int step_num=0x7f070006;
-        public static final int text=0x7f070008;
-        public static final int total_steps=0x7f070004;
+        public static final int alarm_enabled=0x7f070009;
+        public static final int button_test=0x7f070008;
+        public static final int congrat_msg=0x7f070004;
+        public static final int congrat_msg2=0x7f070006;
+        public static final int country_icon=0x7f070000;
+        public static final int logo_index=0x7f070001;
+        public static final int logo_list=0x7f070002;
+        public static final int step_num=0x7f070005;
+        public static final int text=0x7f070007;
+        public static final int total_steps=0x7f070003;
     }
     public static final class layout {
         public static final int activity_game_play=0x7f030000;
@@ -49,8 +48,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int congrat=0x7f050005;
         public static final int easy=0x7f050001;
         public static final int hard=0x7f050003;
+        public static final int index=0x7f050008;
         public static final int medium=0x7f050002;
         public static final int steps_total=0x7f050004;
+        public static final int ubc_logo=0x7f050007;
         public static final int win=0x7f050006;
     }
     public static final class style {
